b62236eef8d5f7ff297af26e4c1bd5c672474c4e
RidanWeb <ridankabir0wr@gmail.com>
2024-08-12 11:42:12 +0600

    Complete Design Part

---
+++ script.js
@@ -0,0 +1,9 @@
+function play(){
+
+    const getHome = document.getElementById('Home');
+    const getPlay = document.getElementById('Play');
+
+
+    getHome.classList.add('hidden');
+    getPlay.classList.remove('hidden');
+}
